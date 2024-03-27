cec60df06d529c73e2347165c42f0073ec979827
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-03-27 05:58:53 -0600

    add testing script

---
+++ .vscode/scripts/copy-script-to-xedit.bat
@@ -0,0 +1,2 @@
+copy "src\scripts\DSDifyer.pas" "C:\Games\Skyrim\Tools\xEdit\Edit Scripts\DSDifyer.pas"
+copy "src\scripts\DSDifyer.pas" "C:\Games\Skyrim\Tools\xEdit Experimental\Edit Scripts\DSDifyer.pas"
