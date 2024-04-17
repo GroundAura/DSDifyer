042d53996be843f6bf10d80a9cc2cc049227d6dc
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-04-16 23:20:06 -0600

    repo updates

--- scripts/copy-scripts.bat
+++ scripts/copy-scripts.bat
@@ -1,4 +1,5 @@
 copy "src\scripts\DSDifyer.pas" "dist\DSDifyer\Edit Scripts\DSDifyer.pas"
+copy "src\scripts\ESP-to-DSD.py" "dist\DSDifyer\ESP-to-DSD.py"
 copy "src\scripts\Generate-Folders.py" "dist\DSDifyer\Generate-Folders.py"
 copy "src\scripts\xEdit-to-DSD.py" "dist\DSDifyer\xEdit-to-DSD.py"
 copy "src\scripts\xTranslator-to-DSD.py" "dist\DSDifyer\xTranslator-to-DSD.py"
